4050f9cb21be3b52dbcef531469067b8eb9b0c14
yeri966 <142251189+yeri966@users.noreply.github.com>
2024-08-27 13:07:19 -0500

    Update Hotel.java

--- co.edu.uniquindio.RepositorioHotel/src/main/java/co/edu/uniquindio/RepositorioHotel/Hotel.java
+++ co.edu.uniquindio.RepositorioHotel/src/main/java/co/edu/uniquindio/RepositorioHotel/Hotel.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.LinkedList;
 
+/*Clase hotel con colecciones de habitaciones, cleintes y reservas*/
 public class Hotel {
     private final Collection<Habitacion> habitaciones;
     private final Collection <Cliente> clientes;
@@ -15,7 +16,7 @@ public class Hotel {
         this.clientes = new LinkedList<>();
         this.reservas = new LinkedList<>();
     }
-
+    /*Metodo el cual nos permite crear habitaciones, si el numero de la habitacion ya existe no se puede crear la habitacion*/
     public void addHabitacion(Habitacion habitacion) {
         if(verificarHabitacionExiste(habitacion.getNumero())) {
             System.out.println("Error: la habitación con número: " + habitacion.getNumero() + "ya existe");
@@ -24,7 +25,8 @@ public class Hotel {
         habitaciones.add(habitacion);
         System.out.println("Habitación número: '" + habitacion.getNumero() + "', de tipo: '" + habitacion.getTipo() + "', y Precio: '" + habitacion.getPrecio() + "' creada correctamente");
     }
-
+    /*  Metodo el cual nos permite agregar un cliente a una habitacion, solo si esta tiene estado de libre
+      y crea la reserva con el numero de la habitacion, el nombre del cliente, la fecha de entrada y salida*/
     public void addClienteHabitacion(Cliente cliente, Habitacion habitacion, int dias) {
         if(habitacion.getEstado() == Estado.LIBRE){
             LocalDateTime fechaEntrada = LocalDateTime.now();
@@ -39,7 +41,7 @@ public class Hotel {
         }
 
     }
-
+    /*Este metodo permite verificar si el numero de la habitacion ya existe*/
     private boolean verificarHabitacionExiste(String numero) {
         for (Habitacion habitacion : habitaciones) {
             if (habitacion.getNumero().equals(numero)) {
@@ -49,19 +51,20 @@ public class Hotel {
         return false;
     }
 
-
+/*Agrega un ciente*/
     public void addCliente(Cliente cliente) {
         clientes.add(cliente);
     }
-
+/*Crea una lista o coleccion de los clientes*/
     public Collection<Cliente> getClientes() {
         return Collections.unmodifiableCollection(clientes);
     }
-
+/*Crea una lista o coleccion de as resenrvas realizadas*/
     public Collection<Reserva> getReservas() {
         return Collections
                 .unmodifiableCollection(reservas);
     }
+/*Crea una lista de las habitaciones */
     public Collection<Habitacion> getHabitaciones() {
         return Collections.unmodifiableCollection(habitaciones);
     }
