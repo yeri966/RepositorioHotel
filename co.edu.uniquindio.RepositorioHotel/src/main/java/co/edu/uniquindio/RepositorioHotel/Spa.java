f3fcccb092ef7610c81b57410ef0ec812fb05694
yeri966 <142251189+yeri966@users.noreply.github.com>
2024-08-27 13:13:45 -0500

    Update Spa.java

--- co.edu.uniquindio.RepositorioHotel/src/main/java/co/edu/uniquindio/RepositorioHotel/Spa.java
+++ co.edu.uniquindio.RepositorioHotel/src/main/java/co/edu/uniquindio/RepositorioHotel/Spa.java
@@ -1,5 +1,5 @@
 package co.edu.uniquindio.RepositorioHotel;
-
+/*Creacion de la clase Spa la cual hereda de la clase Servicio e implementa de la interface Consumible*/
 public class Spa extends Servicio implements Consumible{
     @Override
     public double calcularPrecioServicio() {
